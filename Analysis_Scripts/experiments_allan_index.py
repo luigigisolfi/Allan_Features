e21093b6746e7646560ea1dc587f6f380caeef3e
Luigi Gisolfi <gisolfi.luigi@gmail.com>
2025-04-29 15:21:52 +0200

    Added experiments_allan_index.ipynb notebook

--- Analysis_Scripts/experiments_allan_index.py
+++ Analysis_Scripts/experiments_allan_index.py
@@ -1,3 +1,4 @@
+# %%
 """
 This script allows for plotting of the Modified Allan Deviation per experiment, for all stations
 (equivalent to the MAD plots in Vidhya's paper)
@@ -8,16 +9,20 @@ process_fdets = pride.ProcessFdets() # Create ProcessFdets Object
 utilities = pride.Utilities() # Create Utilities Object
 analysis = pride.Analysis(process_fdets, utilities)  # Create Analysis Object
 
+# %%
 # Experiments to analyze
 experiments_to_analyze = {'juice': ['ec094b']}
 
+# %%
 for mission_name, experiment_names in experiments_to_analyze.items():
     for experiment_name in experiment_names:
-        fdets_folder_path = 'small_dataset/juice/ec094b/input/complete/'
-        output_dir = 'small_dataset/juice/ec094b/input/complete/output/'
+        fdets_folder_path = '../small_dataset/juice/ec094b/input/complete/'
+        output_dir = '../small_dataset/juice/ec094b/output/'
         tau_min = 0
         tau_max = 100
         save_dir = output_dir
         suppress = False
 
         analysis.get_all_stations_madev_plot(fdets_folder_path, experiment_name, tau_min = tau_min, tau_max = tau_max, suppress = False, plot_madev_only = True, save_dir = output_dir)
+
+# %%
